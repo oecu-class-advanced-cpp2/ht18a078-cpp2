d7bfc7950b50d7a47f65e2ce857da79b8199241a
Nogami078 <ht18a078@oecu.jp>
2020-11-17 11:55:08 +0900

    等差数列の生成

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -1,26 +1,29 @@
+// ex_1_main_cpp.cpp
 #include <iostream>
+using namespace std;
 
-#define CPP2_PRIME_UPPER_LIMIT 10000000 //探索する値の上限値
+#define CPP2_PRIME_UPPER_LIMIT 100//探索する値の上限値
 
 /*    --------------------------------------------------------------- */
 /*
- *  nth_prime
- * 
- *  与えられた正整数a と d と n に対して、この等差数列に含まれる n 番目の
- *  素数を返す。 
- *  
- *  TODO: このnth_prime 関数を実装せよ。必要であれば他に関数や 
- *  ファイルを作成せよ。 
- */  
- /*    -------------------------------------------------------------- */
- int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
+*  nth_prime
+*
+*  与えられた正整数a と d と n に対して、この等差数列に含まれる n 番目の
+*  素数を返す。
+*
+*  TODO: このnth_prime 関数を実装せよ。必要であれば他に関数や
+*  ファイルを作成せよ。
+*/
+/*    -------------------------------------------------------------- */
+int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
     for (int i = 0; a + (d*i) < CPP2_PRIME_UPPER_LIMIT; i++) {
         cout << a + (d*i) << endl;
     }
+    return -1;
 }
 
- int main(){
-     std:cout << nth_prime(367, 186, 151) << std:endl;
-     //以下、同様に、出力例通りになるか確認せよ。
-     return 0;
- }
+int main() {
+    nth_prime(2, 3, 100);
+    //以下、同様に、出力例通りになるか確認せよ。
+    return 0;
+}
